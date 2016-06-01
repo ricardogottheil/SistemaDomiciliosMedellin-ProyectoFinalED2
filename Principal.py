2e49f0cdba7d6652f0e75e20c1ab93489ff7afc7
Ricardo Gottheil <ricardogottheil@gmail.com>
2016-05-31 21:55:11 -0500

    Se agrega parte de la nueva implementación del proyecto

--- Principal.py
+++ Principal.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*- 
 
+from Grafo import Arista, Grafo, agregar, caminoMinimo, construirCamino, dijkstra, generarEtiqueta, relacionar
+
 def main():
     """ Funcion principal """
     print "Hola"
