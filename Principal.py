61dcc1567414c416dac78b9658c7d492de6318c0
Ricardo Gottheil <ricardogottheil@gmail.com>
2016-06-06 21:37:30 -0500

    Se agrega ejemplo al archivo Principal.py

--- Principal.py
+++ Principal.py
@@ -5,6 +5,44 @@ from Grafo import Arista, Grafo, agregar, caminoMinimo, construirCamino, dijkstr
 def main():
     """ Funcion principal """
 
+
+    ###  EJEMPLO
+    
+    # a = "a"
+    # b = "b"
+    # c = "c"
+    # d = "d"
+    # e = "e"
+    # f = "f" 
+    # g = "g" 
+    # h = "h"
+
+    # grafo = Grafo()
+    # agregar(grafo, a)
+    # agregar(grafo, b)
+    # agregar(grafo, c)
+    # agregar(grafo, d)
+    # agregar(grafo, e)
+    # agregar(grafo, f)
+    # agregar(grafo, g)
+    # agregar(grafo, h)
+
+    # relacionar(grafo, a, c, 1)
+    # relacionar(grafo, a, b, 3)
+    # relacionar(grafo, b, d, 1)
+    # relacionar(grafo, b, g, 5)
+    # relacionar(grafo, c, f, 5)
+    # relacionar(grafo, c, d, 2)
+    # relacionar(grafo, d, f, 2)
+    # relacionar(grafo, d, e, 4)
+    # relacionar(grafo, e, h, 1)
+    # relacionar(grafo, e, g, 2)
+    # relacionar(grafo, f, h, 3)
+
+    # print caminoMinimo(grafo, e, a)
+
+    # FIN EJEMPLO
+
     #SE CREA EL GRAFO
     grafo = Grafo()
 
@@ -67,7 +105,7 @@ def main():
 
     # AGREGRAR ARCOS
     for v1,v2,n in zip(vertices1,vertices2,nombreArcos):
-        relacionar(grafo, v1, v2, v1)
+        relacionar(grafo, v1, v2, n)
 
     print caminoMinimo(grafo, 0,4)
 
